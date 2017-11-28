02c8006504ebe1cbfebea031bba5084c09ec84d0
elijahprince73 <elijah@1solutionapps.com>
2017-11-28 09:47:21 -0800

    Express Middleware

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
 // Main starting point of application
+const express = require('express');
+const app = express()
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+// App setup
+app.use(morgan('combined'))
+app.use(bodyParser.json({ type: '*/*'}))
+
+// Server setup
+const port = process.env.PORT || 3090
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log('App listening on port:', port))
