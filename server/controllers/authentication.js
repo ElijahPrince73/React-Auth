f3b23ae9d0fea03b1a137b4c691050062a616200
elijahprince73 <elijah@1solutionapps.com>
2017-11-29 10:09:55 -0800

    Searching for users

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,17 @@
-exports.signup = function (req, res, next) {
-  res.send({ success: 'true '})
+const User = require('../models/user');
+
+exports.signup = function(req, res, next) {
+
+  const email = req.body.email
+  const password = req.body.password
+
+  // See if a user with the given email exits
+  User.findOne({
+    email: email
+  }, function(err, existingUser) {})
+  // If user with email does exist, return an error
+
+  // If a user with email does NOT exist, create and save user record
+
+  // Respond to request indicating the user was created
 }
