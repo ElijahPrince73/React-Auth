4d004d940a9afb7d2e23d5d9bcce085f7e21bef0
elijahprince73 <elijah@1solutionapps.com>
2017-11-29 12:14:09 -0800

    Creating User records

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -8,10 +8,23 @@ exports.signup = function(req, res, next) {
   // See if a user with the given email exits
   User.findOne({
     email: email
-  }, function(err, existingUser) {})
-  // If user with email does exist, return an error
+  }, function(err, existingUser) {
+    if (err) {
+      return next(err)
+    }
+    // If user with email does exist, return an error
+    if (existingUser) {
+      return res.status(422).send({error: 'Email is in use'})
+    }
+    // If a user with email does NOT exist, create and save user record
+    const user = new User({email: email, password: password});
 
-  // If a user with email does NOT exist, create and save user record
-
-  // Respond to request indicating the user was created
+    user.save(function(err) {
+      if (err) {
+        return next(err);
+      }
+      // Respond to request indicating the user was created)
+      res.json({success: true})
+    })
+  })
 }
--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
-const Schema = mongoos.Schema
+const Schema = mongoose.Schema
 
 // Define our model
 const userSchema = new Schema({
-  email: { type: String, unique: true, lowercase: true },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true
+  },
   password: String
 })
 
