90dd973e23613bc1c4d341e5188f12a8796c68de
elijahprince73 <elijah@1solutionapps.com>
2017-12-01 08:53:40 -0800

    restart

---
+++ client/webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: ['./src/index.js'],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './',
+    historyApiFallback: true
+  }
+};
