484ec9a235e1eb5494c134bbe997b009da69c603
Elijah Prince <elijahprince73@gmail.com>
2017-12-02 16:40:23 -0800

    Form

--- client/src/components/auth/auth.js
+++ client/src/components/auth/auth.js
@@ -1,25 +1,31 @@
-import React, { Component } from 'react'
-import {reduxForm} from 'redux-form'
-
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
 class Signin extends Component {
-  render() {
+  handleFormValues = values => {
+    console.log(values);
+  };
+  renderInput({ label, ...field }) {
+    console.log(...field.input);
     return (
-      <div>
-        <form>
-          <fieldset className='form-group'>
-            <label className='form-control'>Email</label>
-          </fieldset>
-          <fieldset className='form-group'>
-            <label className='form-control'>Password</label>
-          </fieldset>
-          <button action='submit' className='btn btn-primary'>Sign In</button>
-        </form>
-      </div>
+      <fieldset className="form-group">
+        <label>
+          {label}:
+        </label>
+        <input {...field.input} type="text" className="form-control" />
+      </fieldset>
+    );
+  }
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <form onSubmit={handleSubmit(this.handleFormValues)}>
+        <Field name="email" component={this.renderInput} label="Email" />
+        <Field name="password" component={this.renderInput} label="Password" />
+        <button className="btn btn-primary" action="submit">
+          Sign in
+        </button>
+      </form>
     );
   }
 }
-
-export default reduxForm({
-  form: 'signin',
-  fields: ['email', 'password']
-})(Signin)
+export default reduxForm({ form: 'signin' })(Signin);
