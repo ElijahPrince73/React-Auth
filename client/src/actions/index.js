a2217edbca54e003384e5617da5d4e4bc04956b8
elijahprince73 <elijah@1solutionapps.com>
2017-12-04 10:20:19 -0800

    Displaying errors

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { Route, Redirect } from 'react-router'
-import {AUTH_USER} from './types';
+import {AUTH_USER, AUTH_ERROR} from './types';
 
 const ROOT_URL = 'http://localhost:3090'
 
@@ -12,13 +12,23 @@ export function signinUser({email, password}, history) {
       // If request is good...
       // - Update state to indicate user is authenticated
       dispatch({type: AUTH_USER})
-      // = Save JWT token
+      // - Save JWT token
+      localStorage.setItem('token', response.data.token)
       // - Redirect to '/feature'
       history.push('/feature');
     })
     .catch(() => {
       // If request is bad...
+
       // - Show an error to the user
+      dispatch(authError('Bad Login Info'))
     })
+  }
+}
+
+export function authError(error) {
+  return {
+    type: AUTH_ERROR,
+    payload: error
   }
 }
--- client/src/components/auth/signin.js
+++ client/src/components/auth/signin.js
@@ -8,33 +8,48 @@ class Signin extends Component {
   handleFormValues = values => {
     this.props.signinUser(values, this.props.history)
   };
-  renderInput({ label, ...field }) {
+
+  renderInput({ label, type, ...field }) {
     return (
       <fieldset className="form-group">
         <label>
           {label}:
         </label>
-        <input {...field.input} type="text" className="form-control" />
+        <input {...field.input} type={type} className="form-control" />
       </fieldset>
     );
+  };
+
+  renderAlert(){
+    if (this.props.errorMessage) {
+      return (
+        <div className='alert alert-danger'>
+          <strong>Oops! </strong> {this.props.errorMessage}
+        </div>
+      )
+    }
   }
+
   render() {
     const { handleSubmit } = this.props;
     return (
       <form onSubmit={handleSubmit(this.handleFormValues.bind(this))}>
-        <Field name="email" component={this.renderInput} label="Email" />
-        <Field name="password" component={this.renderInput} label="Password" />
-        <button className="btn btn-primary" action="submit">
-          Sign in
-        </button>
+        <Field name="email" type="text" component={this.renderInput} label="Email" />
+        <Field name="password" type="password" component={this.renderInput} label="Password" />
+        {this.renderAlert()}
+        <button className="btn btn-primary" action="submit">Sign in</button>
       </form>
     );
   }
+}
+
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.error}
 }
 
 export default reduxForm({
   form: 'Signin',
   fields: ['email','password']
 })(
-  connect(null,actions)(withRouter(Signin))
+  connect(mapStateToProps, actions)(withRouter(Signin))
 );
